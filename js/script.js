1fd71cbdde99d897a5fdf06f1a1c95f5279ca506
Noxizt <Noxizt@gmail.com>
2025-03-11 21:52:17 +0800

    2025-03-11 (BUILD 1.0)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,46 @@
-// Инициализация слайдера(SwiperJS)
-new Swiper('.swiper', {
-    // Навигация
-    // Буллеты, текущее положение, прогрессбар
-    
+// Инициализация Свайпера
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 'auto',
     pagination: {
         el: '.swiper-pagination',
-        clickable: true, // клибельность
-        
+        clickable: true,
     },
-    // Авто-высота
-  
-    // slidesPerView: 2,
-    loop: true,
-    slidesPerView:'auto',
-    spaceBetween: 16,
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
+    breakpoints: {
+        0: { // при ширине экрана 0px и выше:
+            enabled: true, // включение свайпера
+            autoHeight: true // автоматическая высота swiper-wrapper от высоты слайда (обратить внимание на зависимость параметров swiper-wrapper от swiper)
+        },
+        768: {
+            enabled: false,
+            autoHeight: true
+        }
+    }
 });
+
+// Свайпер при ресайзе
+window.addEventListener('resize', function () {
+    if (window.innerWidth < 768) {
+        // swiper.updateAutoHeight(1);
+        swiper.enable();
+    } else if (window.innerWidth >= 768) {
+
+        swiper.realIndex = 0; // устанавка активного слайда по индексу
+        swiper.disable();
+        swiper.updateAutoHeight(1);
+    }
+});
+
+// Expand кнопка
+document.querySelector('.button-expand').addEventListener('click', () => {
+    if (document.querySelector('.button-expand_inner').innerHTML === 'Показать все') {
+        document.querySelector('.swiper.swiper-custom').style.height = '272px';
+        document.querySelector('.button-expand_inner').innerHTML = 'Скрыть';
+    } else if (document.querySelector('.button-expand_inner').innerHTML === 'Скрыть') {
+        document.querySelector('.swiper.swiper-custom').style.height = '184px';
+        document.querySelector('.button-expand_inner').innerHTML = 'Показать все';
+    }
+});
+
+
+
